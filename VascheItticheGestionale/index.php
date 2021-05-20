070398179fd36883051d8b5b2d95c48289852146
Jacopo Mattia Marconi <73340924+JacopoMattiaMarconi@users.noreply.github.com>
2021-05-21 00:07:35 +0200

    Add files via upload

---
+++ VascheItticheGestionale/index.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link rel="stylesheet" href="StiliLogin.css" type="text/css">
+</head>
+<body style="background-image: url(valeriia-miller-Dew8ESyRzUw-unsplash.jpg);">
+
+	<div class="sidenav">
+         <div class="login-main-text">
+            <h2>Area riservata<br>Gestione personale aziendale</h2>
+            <p>Accedi all'area riservata.</p>
+         </div>
+    </div>
+	
+      <div class="main">
+         <div class="col-md-5 col-sm-12">
+            <div class="login-form">
+               <form>
+                  <div class="form-group">
+                     <label>Mail</label>
+                     <input type="text" class="form-control" placeholder="Mail">
+                  </div>
+                  <div class="form-group">
+                     <label>Password</label>
+                     <input type="password" class="form-control" placeholder="Password">
+                  </div>
+                  <button type="submit" class="btn btn-black">Accedi</button>
+               </form>
+            </div>
+         </div>
+      </div>
+	  
+</body>
+</html>
---
+++ VascheItticheGestionale/modelli.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<link rel="stylesheet" href="Stili.css" type="text/css">
+</head>
+<body style="background-image: url(valeriia-miller-Dew8ESyRzUw-unsplash.jpg);">
+<?php 
+include 'menu4.php';
+?>
+<center>
+
+<div class="w-50 p-3" style="background-color: #e3f2fd;">
+
+</div>
+
+<br><br><br>
+</center>
+</body>
+</html>
